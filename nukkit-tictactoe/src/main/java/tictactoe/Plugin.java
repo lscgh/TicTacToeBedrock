c9b12cdff26710ba3dfe4459afb169882ed2dcb2
levischnorr <levischnorr@web.de>
2024-02-11 09:56:34 +0100

    Project setup

---
+++ nukkit-tictactoe/src/main/java/tictactoe/Plugin.java
@@ -0,0 +1,12 @@
+package tictactoe;
+
+import cn.nukkit.plugin.PluginBase;
+
+public class Plugin extends PluginBase {
+
+	@Override
+	public void onLoad() {
+		this.getLogger().info("TicTacToe plugin loaded!");
+	}
+	
+}
